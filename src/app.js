885d0cea3ec3265a67bf3abf40619d789ca54133
Tbnelly <nellytobiloba@gmail.com>
2026-04-16 20:12:51 +0100

    fix: making it serverless

--- src/app.js
+++ src/app.js
@@ -5,19 +5,13 @@ require("dotenv").config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// DB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB error:", err));
+mongoose.connect(process.env.MONGO_URI);
 
-// routes
 app.use("/api", require("./routes/profileRoutes"));
 
-// error handlers
 const { errorHandler, notFoundHandler } = require("./middleware/errorHandler");
 app.use(notFoundHandler);
 app.use(errorHandler);
